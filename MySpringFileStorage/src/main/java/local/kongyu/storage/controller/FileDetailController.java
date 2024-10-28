967cca31b0b569f7b2901753540f64a8537d6616
孔余 <2385569970@qq.com>
2024-10-28 21:35:13 +0800

    新增FTP

--- MySpringFileStorage/src/main/java/local/kongyu/storage/controller/FileDetailController.java
+++ MySpringFileStorage/src/main/java/local/kongyu/storage/controller/FileDetailController.java
@@ -1,13 +1,17 @@
 package local.kongyu.storage.controller;
 
+import cn.hutool.core.util.StrUtil;
 import org.dromara.x.file.storage.core.FileInfo;
 import org.dromara.x.file.storage.core.FileStorageService;
+import org.dromara.x.file.storage.core.get.ListFilesResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import java.nio.charset.Charset;
 
 @RestController
 public class FileDetailController {
@@ -24,6 +28,14 @@ public class FileDetailController {
     }
 
     /**
+     * 上传文件 指定平台
+     */
+    @PostMapping("/upload-ftp")
+    public FileInfo uploadFtp(MultipartFile file) {
+        return fileStorageService.of(file).setPlatform("ftp-1").upload();
+    }
+
+    /**
      * 上传文件，成功返回文件 url
      */
     @PostMapping("/upload2")
@@ -32,7 +44,7 @@ public class FileDetailController {
                 .setPath("upload/") //保存到相对路径下，为了方便管理，不需要可以不写
                 .setObjectId("0")   //关联对象id，为了方便管理，不需要可以不写
                 .setObjectType("0") //关联对象类型，为了方便管理，不需要可以不写
-                .putAttr("role","admin") //保存一些属性，可以在切面、保存上传记录、自定义存储平台等地方获取使用，不需要可以不写
+                .putAttr("role", "admin") //保存一些属性，可以在切面、保存上传记录、自定义存储平台等地方获取使用，不需要可以不写
                 .upload();  //将文件上传到对应地方
         return fileInfo == null ? "上传失败！" : fileInfo.getUrl();
     }
@@ -44,8 +56,8 @@ public class FileDetailController {
     @PostMapping("/upload-image")
     public FileInfo uploadImage(MultipartFile file) {
         return fileStorageService.of(file)
-                .image(img -> img.size(1000,1000))  //将图片大小调整到 1000*1000
-                .thumbnail(th -> th.size(200,200))  //再生成一张 200*200 的缩略图
+                .image(img -> img.size(1000, 1000))  //将图片大小调整到 1000*1000
+                .thumbnail(th -> th.size(200, 200))  //再生成一张 200*200 的缩略图
                 .upload();
     }
 
@@ -66,6 +78,28 @@ public class FileDetailController {
     @PostMapping("/upload-request")
     public FileInfo uploadPlatform(HttpServletRequest request) {
         return fileStorageService.of(request).upload();
+    }
+
+
+    @GetMapping("/read-ftp")
+    public void readFtp() {
+        getFtpFileList("sql");
+    }
+
+    public void getFtpFileList(String filePrefix) {
+        // 列出文件
+        ListFilesResult files = fileStorageService.listFiles().setPlatform("ftp-1").setFilenamePrefix(filePrefix).listFiles();
+        files.getFileList().forEach(file -> {
+            FileInfo fileInfo = file.toFileInfo();
+            String filename = fileInfo.getFilename();
+            try {
+                byte[] fileBytes = fileStorageService.download(fileInfo).bytes();
+                String data = StrUtil.str(fileBytes, Charset.defaultCharset());
+                System.out.println(StrUtil.format("文件[{}]={}", filename, data));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
 
